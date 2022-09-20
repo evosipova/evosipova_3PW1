cdc08b0a0d90551e042fc98fc55f58305144ee30
evosipova <90273831+evosipova@users.noreply.github.com>
2022-09-20 14:14:30 +0300

    homework 1

--- evosipova_3PW1/ViewController.swift
+++ evosipova_3PW1/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var views: [UIView]!
+    @IBAction func button(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        basicFunctionality()
     }
+    
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        var set = Set<UIColor>()
+        while set.count < views.count {
+            set.insert(
+                UIColor(
+                    red: .random(in: 0...1),
+                    green: .random(in: 0...1),
+                    blue: .random(in: 0...1),
+                    alpha: 1
+                )
+            )
+        }
+        let button = sender as? UIButton
+        button?.isEnabled = false
+        UIView.animate(withDuration: 2, animations: {
+            for view in self.views {
+                view.layer.cornerRadius = CGFloat(Int.random(in: 5..<25))
+                view.backgroundColor = set.popFirst()
 
-
+            }
+        }) { completion in
+            button?.isEnabled = true
+        }
+    }
+    
+    func basicFunctionality(){
+        var set = Set<UIColor>()
+        while set.count < views.count {
+            set.insert(
+                UIColor(
+                    red: .random(in: 0...1),
+                    green: .random(in: 0...1),
+                    blue: .random(in: 0...1),
+                    alpha: 1
+                )
+            )
+        }
+        UIView.animate(withDuration: 2, animations: {
+            for view in self.views {
+                view.layer.cornerRadius = CGFloat(Int.random(in: 5..<20))
+                view.backgroundColor = set.popFirst()
+            }
+        })
+    }
 }
-

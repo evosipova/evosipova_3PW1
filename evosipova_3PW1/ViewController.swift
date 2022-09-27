4fe8a4ae91f18e7c2311b965d39e5cb57e0d5ea6
evosipova <90273831+evosipova@users.noreply.github.com>
2022-09-27 23:37:30 +0300

    HW1

--- evosipova_3PW1/ViewController.swift
+++ evosipova_3PW1/ViewController.swift
@@ -14,51 +14,50 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        basicFunctionality()
+        var colors = getRandomColorsSet()
+        for view in views{
+            view.layer.cornerRadius = .random(in: 0...40)
+            view.backgroundColor = colors.popFirst()
+        }
     }
     
-    @IBAction func changeColorButtonPressed(_ sender: Any) {
-        var set = Set<UIColor>()
-        while set.count < views.count {
-            set.insert(
-                UIColor(
-                    red: .random(in: 0...1),
-                    green: .random(in: 0...1),
-                    blue: .random(in: 0...1),
-                    alpha: 1
-                )
-            )
+    func getRandomColorsSet() -> Set<UIColor>{
+        var colorsArray = Set<UIColor>()
+        var num: UInt64 = 0
+        while (colorsArray.count < views.count) {
+            var color = "#"
+            let symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a","b","c","d","e","f"]
+            while (color.count < 9) {
+                color += String(symbols[Int.random(in: 0..<16)])
+            }
+            let scanner = Scanner(string:
+                                    String(color[color.index(color.startIndex,
+                                                             offsetBy:1)...]))
+            scanner.scanHexInt64(&num)
+            colorsArray.insert(UIColor(red: CGFloat((num & 0xff000000) >> 24) / 255,
+                                  green: CGFloat((num & 0x00ff0000) >> 16) / 255,
+                                  blue: CGFloat((num & 0x0000ff00) >> 8) / 255,
+                                  alpha: CGFloat(num & 0x000000ff) / 255))
         }
+        return colorsArray
+    }
+    
+    
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        var set =  getRandomColorsSet()
         let button = sender as? UIButton
         button?.isEnabled = false
         UIView.animate(withDuration: 2, animations: {
             for view in self.views {
-                view.layer.cornerRadius = CGFloat(Int.random(in: 5..<25))
+                view.layer.cornerRadius = CGFloat(Int.random(in: 5..<20))
                 view.backgroundColor = set.popFirst()
-
+                view.transform = CGAffineTransform(
+                                        scaleX: CGFloat.random(in: -2...2),
+                                            y: CGFloat.random(in: -2...2)
+                                    )
             }
         }) { completion in
             button?.isEnabled = true
         }
-    }
-    
-    func basicFunctionality(){
-        var set = Set<UIColor>()
-        while set.count < views.count {
-            set.insert(
-                UIColor(
-                    red: .random(in: 0...1),
-                    green: .random(in: 0...1),
-                    blue: .random(in: 0...1),
-                    alpha: 1
-                )
-            )
-        }
-        UIView.animate(withDuration: 2, animations: {
-            for view in self.views {
-                view.layer.cornerRadius = CGFloat(Int.random(in: 5..<20))
-                view.backgroundColor = set.popFirst()
-            }
-        })
     }
 }
